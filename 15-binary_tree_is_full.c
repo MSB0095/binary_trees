9c2469192b9538a36d3da8f9b3c61fb8845ab818
MSB0095 <andromedianmsb01@gmail.com>
2024-06-07 02:37:44 +0100

    chore: Refactor binary_tree_is_full function to improve readability

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -17,9 +17,6 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		{
 			return (1);
 		}
-		else
-		{
-			return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-		}
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 	}
 }
